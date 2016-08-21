a04b339e32eb648caaf147490fa7af2ff9c1775d
Jorge Pabon <pianistapr@hotmail.com>
2016-08-21 04:47:50 -0400

    Fix spaces
    Fix Spaces for readability

--- change-network-settings.sh
+++ change-network-settings.sh
@@ -51,10 +51,11 @@ function agree()
 	echo 
 	echo -e $RED'1. This script has been tested on Ubuntu (Server & Desktop).'
 	echo -e '2. The author(s) cannot be held accountable for any problems that might occur if you run this script.'
-	echo -e '3. Proceed only if you authorize this script to make changes to your system.'$BLACK
+	echo -e '3. Proceed only if you authorize this script to make changes to your system.'
 	echo
 
-	echo -e 'Continue to AGREE.  Otherwise press [CTRL + C]'
+	echo -e 'CONTINUE TO AGREE.  OTHERWISE PRESS    [ C T R L   +   C ]'$BLACK
+	echo
 }
 
 ########################### Prompt for the Interface to edit ###########################
