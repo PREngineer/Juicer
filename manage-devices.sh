d224cc2b561838ede3e0974d236fa748c2cd5c1d
Jorge Pabon <pianistapr@hotmail.com>
2017-12-13 19:05:34 -0500

    test

--- manage-devices.sh
+++ manage-devices.sh
@@ -131,7 +131,7 @@ function show_drives()
   echo -e
   echo -e $GREEN'mmcblk = Micro SD'$CYAN' | Other = USB'
   echo -e
-  echo -e $BLACK'Drive\t\tSpace\tUsed\tAvail\t%Used\tMounted'
+  echo -e $BLACK'Drive\t\tSpace Used\tAvail\t%Used\tMounted'
 	echo -e '-----------------------------------------------'
 	df -h | grep '/dev/'
 	echo -e '-----------------------------------------------'
